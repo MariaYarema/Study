1bfdb246e04bba99db1d5db90e52fd34be3f7aea
Maria <97191910+MariaYarema@users.noreply.github.com>
2022-01-06 10:29:08 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp2// Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!cvcv");
+        }
+    }
+}
